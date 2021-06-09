ccca55dead98bd05dc689310da2f215d7bbf31bb
Mikkel Bernhof Jakobsen <bernhof@gmail.com>
2021-06-09 14:16:37 +0200

    Anvend SIT VPN med custom openconnect klient

--- erst/erst.custom.bash
+++ erst/erst.custom.bash
@@ -6,24 +6,15 @@ export DOCKER_HOST="tcp://127.0.0.1:2375"
 
 # Connect to ERST VPN using openconnect and 1Password
 vpn() {
-    sudo echo "sudo OK"
-    if [ "$?" -ne "0" ]; then
-        return $?
-    fi
     local itemname username passcode
-    itemname="^ERST VPN$"
-    # log into 1password
-    opin
-    # abort if login fails
-    if [ "$?" -ne "0" ]; then
-        return $?
-    fi
-    # get username from 1password
-    username=$(opfield "$itemname" username)
-    # pipe password on stdin
-    # note: since openconnect expects the second line on stdin to specify the passcode,
-    # we need to create an empty prompt for this, which will pause openconnect while it
-    # waits for user input:
-    (opfield "$itemname" password; read -s passcode; echo $passcode) | \
-        sudo openconnect vpn.erst.dk -u "$username" --passwd-on-stdin
+    sudo echo "sudo OK" &&
+        itemname="^ERST SIT VPN$" &&
+        opin && # log into 1password
+        username=$(opfield "$itemname" username) && # get username from 1password
+        # pipe password on stdin
+        # note: since openconnect expects the second line on stdin to specify the passcode,
+        # we need to create an empty prompt for this, which will pause openconnect while it
+        # waits for user input:
+        (opfield "$itemname" password; read -s passcode; echo $passcode) | \
+            sudo ~/src/gitlab/openconnect/openconnect-add_local_id_option/openconnect ext2.statens-it.dk --passwd-on-stdin --user "$username" --local-id device_uniqueid="`lsblk -r -o mountpoint,uuid |  grep '^/ ' |  cut -c3- |  tr -d '\n' | sha1sum | cut -d\  -f1 | tr '[:lower:]' '[:upper:]'`"
 }
