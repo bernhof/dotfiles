9a020f0722f13c7fe2deaf9259d2db9c2b4d82ef
Mikkel Bernhof Jakobsen <bernhof@gmail.com>
2022-01-04 22:00:44 +0100

    Remove docker "fixes"

--- erst/erst.install.bash
+++ erst/erst.install.bash
@@ -5,21 +5,10 @@ sudo snap install teams
 
 sdk install java 8.0.265-open
 
-# See also: http://confluence/display/MIT/Kom+Godt+I+Gang+Som+Udvikler
-sudo ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
-
-# Docker setup, see http://confluence/pages/viewpage.action?pageId=36241500
-
-sudo sed -i 's/ExecStart=\/usr\/bin\/dockerd -H fd:\/\//ExecStart=\/usr\/bin\/dockerd/' /lib/systemd/system/docker.service
-# WARNING: this overwrites any existing config entirely (there shouldn't be one initially, though)
-echo '{ "bip": "10.20.0.1/16", "hosts": ["tcp://127.0.0.1:2375"] }' | sudo tee /etc/docker/daemon.json
-
-sudo systemctl daemon-reload
-sudo service docker restart
-
 # Install redis:
 sudo apt install make clang -y # allows building redis-cli from source
 make distclean                 # avoids build errors
+
 # Source: https://codewithhugo.com/install-just-redis-cli-on-ubuntu-debian-jessie/
 cd /tmp
 wget http://download.redis.io/redis-stable.tar.gz
@@ -30,4 +19,4 @@ sudo cp src/redis-cli /usr/local/bin/
 sudo chmod 755 /usr/local/bin/redis-cli
 cd ~
 
-google-chrome http://confluence/x/2oGG # Kom godt i gang som udvikler
+google-chrome https://github.com/kaikramer/keystore-explorer/releases/latest # Ikke pt. tilgængelig som pakke på apt/snap
